a4543802e523822cb2c2e5e0a8809f9dfe887038
Thom low <thomthomlow@gmail.com>
2023-08-18 00:11:58 +0800

    18 august
    adding all the unity object and textures into unity

---
+++ 3d modelling world/Assets/script/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.script
+{
+    public class MoveCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform m_Camera;
+
+        void Update()
+        {
+            transform.position = m_Camera.position;
+        }
+    }
+}
---
+++ 3d modelling world/Assets/script/PlayerCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.script
+{
+    public class PlayerCamera : MonoBehaviour
+    {
+        public float sensex;
+        public float sensey;
+
+        public Transform orientation;
+
+        private float XRotation;
+        private float YRotation;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensex;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensey;
+
+            YRotation += mouseX;
+            XRotation -= mouseY;
+
+            XRotation = Mathf.Clamp(XRotation, -90f , 90f);
+            transform.rotation = Quaternion.Euler(XRotation, YRotation, 0);
+            orientation.rotation = Quaternion.Euler(0,YRotation, 0);
+        }
+    }
+}
